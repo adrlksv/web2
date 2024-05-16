0c83d612416b4b184b7d94a75241c9e653d905d8
adrlksv <Qwerty123Andrey@bk.ru>
2024-05-16 21:35:40 +0300

    tasks 6-7

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+// const inter = Inter({subsets: ['latin']})
+
+// export default function Home() {
+//     return (
+//         <main className={StyleSheet.main}>
+//             <div>Index page</div>
+//         </main>
+//     )
+// }
